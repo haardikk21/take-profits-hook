d0f3cb5f61a75eda67596f8bd5d67aba9fe1c3ab
Haardik <haardik@haardik.dev>
2024-04-24 13:47:51 -0400

    dont pack positionId encoding

--- src/TakeProfitsHook.sol
+++ src/TakeProfitsHook.sol
@@ -293,16 +293,25 @@ contract TakeProfitsHook is BaseHook, ERC1155 {
         int24 tick,
         bool zeroForOne
     ) public pure returns (uint256) {
-        return
-            uint256(keccak256(abi.encodePacked(key.toId(), tick, zeroForOne)));
+        return uint256(keccak256(abi.encode(key.toId(), tick, zeroForOne)));
     }
 
     function getLowerUsableTick(
         int24 tick,
         int24 tickSpacing
     ) private pure returns (int24) {
+        // E.g. tickSpacing = 60, tick = -100
+        // closest usable tick rounded-down will be -120
+
+        // intervals = -100/60 = -1 (integer division)
         int24 intervals = tick / tickSpacing;
+
+        // since tick < 0, we round `intervals` down to -2
+        // if tick > 0, `intervals` is fine as it is
         if (tick < 0 && tick % tickSpacing != 0) intervals--; // round towards negative infinity
+
+        // actual usable tick, then, is intervals * tickSpacing
+        // i.e. -2 * 60 = -120
         return intervals * tickSpacing;
     }
 }
